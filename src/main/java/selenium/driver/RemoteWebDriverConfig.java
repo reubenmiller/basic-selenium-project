73b65e1958ab47b788d04d64a5e208319a2d60f2
reuben.d.miller@gmail.com <reuben.d.miller@gmail.com>
2019-01-09 10:20:19 +0100

    Adding support for setting other properties via system.properties and env vars

--- src/main/java/selenium/driver/RemoteWebDriverConfig.java
+++ src/main/java/selenium/driver/RemoteWebDriverConfig.java
@@ -5,7 +5,7 @@ import selenium.configurations.TypedProperties;
 public class RemoteWebDriverConfig {
         private final TypedProperties typedProperties = new TypedProperties("/driver_config.properties");
 
-        String getPropertyValue(String name) {
+        String getPropertyString(String name) {
                 String value = System.getenv(name.toUpperCase().replace(".", "_"));
 
                 if (!value.matches(".+")) {
@@ -17,23 +17,34 @@ public class RemoteWebDriverConfig {
                 return value;
         }
 
+        int getPropertyInt(String name) {
+                String value = this.getPropertyString(name);
+                int valueInt = 0;
+                
+                try {
+                        valueInt = Integer.parseInt(value);
+                } catch (Exception Ex) {}
+
+                return valueInt;
+        }
+
         String getBrowserName() {
-                return typedProperties.getValue("browser.name");
+                return this.getPropertyString("browser.name");
         }
 
         int getImplicitlyWait() {
-                return typedProperties.getInt("implicitly_wait");
+                return this.getPropertyInt("implicitly_wait");
         }
 
         int getDomMaxScriptRunTime() {
-                return typedProperties.getInt("dom.max_script_run_time");
+                return this.getPropertyInt("dom.max_script_run_time");
         }
 
         String getRemoteWebDriver() {
-                return typedProperties.getValue("remote_webdriver");
+                return this.getPropertyString("remote_webdriver");
         }
 
         String getBrowserProxy() {
-                return this.getPropertyValue("browser.proxy");
+                return this.getPropertyString("browser.proxy");
         }
 }
